dc60c0a688bb17b446d0833750eb3e15823dcbf1
Norbert Yemuang Bope <irnorbertyemuang@gmail.com>
2023-04-03 14:55:14 +0100

    fiveth commit :add menu page

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+const mobileMenuEvent = ()=>{
+    document.querySelector('#mobile-checkbox').addEventListener('change',(e)=>{
+         document.querySelector('.menu-mobile').classList.toggle('menu-mobile-active')
+    })
+}
+
+window.addEventListener( "scroll", ( ) => {
+    if ( window.scrollY >= 100 ) {
+        document.querySelector( ".container__header" ).classList.add( "header-bg-visible" )
+    } else {
+        document.querySelector( ".container__header" ).classList.remove( "header-bg-visible" )
+    }
+} )
+
+window.addEventListener( "load", ( ) => {
+    if ( window.scrollY >= 100 ) {
+        document.querySelector( ".container__header" ).classList.add( "header-bg-visible" )
+    } else {
+        document.querySelector( ".container__header" ).classList.remove( "header-bg-visible" )
+    }
+    mobileMenuEvent();
+    document.querySelector('.loader-page').style.display ="none"
+} )
