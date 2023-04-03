4db3105167d3ddd605d5f7ae34f2bcccc9742484
Norbert Yemuang Bope <irnorbertyemuang@gmail.com>
2023-04-03 13:28:58 +0100

    second commit : responsive design css

--- js/home.js
+++ js/home.js
@@ -1,3 +1,9 @@
+const mobileMenuEvent = ()=>{
+    document.querySelector('#mobile-checkbox').addEventListener('change',(e)=>{
+         document.querySelector('.menu-mobile').classList.toggle('menu-mobile-active')
+    })
+}
+
 window.addEventListener( "scroll", ( ) => {
     if ( window.scrollY >= 100 ) {
         document.querySelector( ".container__header" ).classList.add( "header-bg-visible" )
@@ -12,4 +18,5 @@ window.addEventListener( "load", ( ) => {
     } else {
         document.querySelector( ".container__header" ).classList.remove( "header-bg-visible" )
     }
+    mobileMenuEvent()
 } )
