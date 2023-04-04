89f5e1a6c7562f408cfe341fe7a50784e53a1f6e
Norbert Yemuang Bope <irnorbertyemuang@gmail.com>
2023-04-04 14:49:50 +0100

    update mobileMenuEvent at all js file

--- js/home.js
+++ js/home.js
@@ -1,7 +1,16 @@
-const mobileMenuEvent = ()=>{
-    document.querySelector('#mobile-checkbox').addEventListener('change',(e)=>{
-         document.querySelector('.menu-mobile').classList.toggle('menu-mobile-active')
-    })
+const mobileMenuEvent = ( ) => {
+    document.querySelector( '#mobile-checkbox' ).addEventListener( 'change', ( e ) => {
+        if ( document.querySelector( '.menu-mobile' ).classList.contains( 'menu-mobile-active' ) ) {
+            if ( window.scrollY <= 100 ) {
+                document.querySelector( ".container__header" ).classList.remove( "header-bg-visible" )
+            }
+        } else {
+            if ( window.scrollY <= 100 ) {
+                document.querySelector( ".container__header" ).classList.add( "header-bg-visible" )
+            }
+        }
+        document.querySelector( '.menu-mobile' ).classList.toggle( 'menu-mobile-active' )
+    } )
 }
 
 window.addEventListener( "scroll", ( ) => {
